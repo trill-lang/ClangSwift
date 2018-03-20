a60ff18fd3334ac4e40d46ddc8d1302dc33e0deb
achraf <achraf.mamdouh@yahoo.com>
2018-03-20 16:09:36 +0000

    Adds saveTranslationUnit(in:withOptions:)
    Adds ClangSaveError
    Adds TranslationUnitSaveOptions
    Updates defunct testInitFromASTFile

--- Tests/ClangTests/ClangTests.swift
+++ Tests/ClangTests/ClangTests.swift
@@ -71,10 +71,19 @@ class ClangTests: XCTestCase {
 
   func testInitFromASTFile() {
     do {
-      let filename = "input_tests/init-ast.ast"
-      let unit = try TranslationUnit(astFilename: filename)
+      let filename = "input_tests/init-ast.c"
+      let astFilename = "/tmp/JKN-23-AC.ast"
+
+      let unit = try TranslationUnit(filename: filename)
+      try unit.saveTranslationUnit(in: astFilename,
+                                   withOptions: unit.defaultSaveOptions)
+      defer {
+        try? FileManager.default.removeItem(atPath: astFilename)
+      }
+
+      let unit2 = try TranslationUnit(astFilename: astFilename)
       XCTAssertEqual(
-        unit.tokens(in: unit.cursor.range).map { $0.spelling(in: unit) },
+        unit2.tokens(in: unit2.cursor.range).map { $0.spelling(in: unit2) },
         ["int", "main", "(", "void", ")", "{", "return", "0", ";", "}"]
       )
     } catch {
